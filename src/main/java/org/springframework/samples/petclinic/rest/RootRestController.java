13c57b577a9fb68a915551a962911e649b43fc8e
larsobist <48766930+larsobist@users.noreply.github.com>
2021-02-01 13:45:14 +0100

    Basic konfigurationen

--- src/main/java/org/springframework/samples/petclinic/rest/RootRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/RootRestController.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,12 +34,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RootRestController {
 
-    @Value("#{servletContext.contextPath}")
-    private String servletContextPath;
-
 	@RequestMapping(value = "/")
 	public void redirectToSwagger(HttpServletResponse response) throws IOException {
-		response.sendRedirect(this.servletContextPath + "/swagger-ui.html");
+		response.sendRedirect("/petclinic/swagger-ui.html");
 	}
 
 }
