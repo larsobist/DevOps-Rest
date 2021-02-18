f0195f718aa83bb575881d856bed5e3dc8f87a97
scorpgyal <julia.litvin.jl@gmail.com>
2021-02-18 15:18:19 +0100

    VisitRestControllerTest.java -> isOk()

--- src/test/java/org/springframework/samples/petclinic/rest/VisitRestControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/rest/VisitRestControllerTests.java
@@ -210,7 +210,7 @@ public class VisitRestControllerTests {
     	this.mockMvc.perform(put("/api/visits/2")
     		.content(newVisitAsJSON).accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE))
         	.andExpect(content().contentType("application/json"))
-        	.andExpect(status().isNoContent());
+        	.andExpect(status().isOk());
 
     	this.mockMvc.perform(get("/api/visits/2")
            	.accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON_VALUE))
