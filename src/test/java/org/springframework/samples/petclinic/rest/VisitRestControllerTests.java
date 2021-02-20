ebddd0104f607e439536794ce6b62f51dae60018
scorpgyal <julia.litvin.jl@gmail.com>
2021-02-20 13:28:11 +0100

    Update VisitRestControllerTests.java

--- src/test/java/org/springframework/samples/petclinic/rest/VisitRestControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/rest/VisitRestControllerTests.java
@@ -157,7 +157,7 @@ public class VisitRestControllerTests {
     @WithMockUser(roles="OWNER_ADMIN")
     public void testGetVisitsByVetSuccess() throws Exception {
     	given(this.clinicService.getVisitsByVet(1)).willReturn(visits);
-        this.mockMvc.perform(get("/api/vetVisits/1")
+        this.mockMvc.perform(get("/api/visits/vetVisits/1")
         	.accept(MediaType.APPLICATION_JSON_VALUE))
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json"))
